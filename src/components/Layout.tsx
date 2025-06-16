67706f088504db114883cd953f2e4542fda4b359
Lobster444 <misanya111@gmail.com>
2025-06-16 18:13:09 +0100

    Fix Header component imports to use default import

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-import { Header } from './Header'
+import Header from './Header'
 import { MobileNav } from './MobileNav'
 import { ConnectionBanner } from './ui/ConnectionBanner'
 import { useAuth } from '../hooks/useAuth'
